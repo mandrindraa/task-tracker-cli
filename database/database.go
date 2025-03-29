f61a9aeab30b377789ce253e62067868898c07cd
mandrindraa <mandrindraantonnio@gmail.com>
2025-03-29 20:05:21 +0300

    Database in a singleton pattern

---
+++ database/database.go
@@ -0,0 +1,27 @@
+package database
+
+import (
+	"fmt"
+	"os"
+	"sync"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+var (
+	db     *gorm.DB
+	dbOnce sync.Once
+)
+
+func GetDB() *gorm.DB {
+	dbOnce.Do(func() {
+		var err error
+		db, err = gorm.Open(sqlite.Open("task.db"), &gorm.Config{})
+		if err != nil {
+			fmt.Println("Error connecting to the database:", err)
+			os.Exit(1)
+		}
+	})
+	return db
+}
