209d89ffa8d2ea33d3cfaec7f25f7f669e803884
mandrindraa <mandrindraantonnio@gmail.com>
2025-03-30 12:08:14 +0300

    Do no use transaction to improve perfomance

--- database/database.go
+++ database/database.go
@@ -17,7 +17,9 @@ var (
 func GetDB() *gorm.DB {
 	dbOnce.Do(func() {
 		var err error
-		db, err = gorm.Open(sqlite.Open("task.db"), &gorm.Config{})
+		db, err = gorm.Open(sqlite.Open("task.db"), &gorm.Config{
+			SkipDefaultTransaction: true,
+		})
 		if err != nil {
 			fmt.Println("Error connecting to the database:", err)
 			os.Exit(1)
