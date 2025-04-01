db2fb61f60aaaa0d40390f2d1a790345037f22bd
mandrindraa <mandrindraantonnio@gmail.com>
2025-04-01 15:06:06 +0300

    verify the validity of the status provided by the user

--- models/status.go
+++ models/status.go
@@ -1,9 +1,18 @@
 package models
 
 const (
-	Pending    string = "pending"
+	Pending    string = "pending    "
 	InProgress string = "in progress"
-	Completed  string = "completed"
-	Aborted    string = "aborted"
-	ToDo       string = "to do"
+	Completed  string = "completed  "
+	Aborted    string = "aborted    "
+	ToDo       string = "to do      "
 )
+
+func IsValidStatus(status string) bool {
+	switch status {
+	case Pending, InProgress, Completed, Aborted, ToDo:
+		return true
+	default:
+		return false
+	}
+}
